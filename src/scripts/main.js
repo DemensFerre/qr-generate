8e3cbf99f1bff6b46b9d115dc7f1e55622d674fa
unknown <thewildstar.m@gmail.com>
2022-07-20 13:57:18 +0300

    Добавлен функционал на js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
+const wrapper = document.querySelector(".wrapper");
+const form = wrapper.querySelector(".wrapper__form");
+const qrInput = form.querySelector(".wrapper__qr-input");
+const qrButton = form.querySelector(".wrapper__qr-button");
+const qrImg = wrapper.querySelector(".wrapper__qr-code img");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const qrInputValue = qrInput.value;
+
+  if (!qrInputValue) return;
+
+  qrButton.innerText = "Создаем QR-код...";
+
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrInputValue}`;
+
+  qrImg.addEventListener("load", () => {
+    wrapper.classList.add("active");
+    qrButton.innerText = "Создать QR-код";
+  });
+});
