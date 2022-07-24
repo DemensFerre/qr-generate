da439322f826573b5b9f38abcf251c3e67b98a36
unknown <thewildstar.m@gmail.com>
2022-07-24 12:13:32 +0300

    добавлена возможеность выбора размера qr-кода

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,24 @@ const form = wrapper.querySelector(".wrapper__form");
 const qrInput = form.querySelector(".wrapper__qr-input");
 const qrButton = form.querySelector(".wrapper__qr-button");
 const qrImg = wrapper.querySelector(".wrapper__qr-code img");
+const radioButtons = form.querySelectorAll(".radio__input");
 
 const viewQR = () => {
   const qrInputValue = qrInput.value;
+
+  let radioValue;
 
   if (!qrInputValue) return;
 
   qrButton.innerText = "Создаем QR-код...";
 
-  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrInputValue}`;
+  radioButtons.forEach((item) => {
+    if (item.checked) {
+      radioValue = item.value;
+    }
+  });
+
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=${radioValue}x${radioValue}&data=${qrInputValue}`;
 
   qrImg.addEventListener("load", () => {
     wrapper.classList.add("active");
